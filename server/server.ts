26ad6ad55a10859a3265fa6e13b609e178ed46bb
Aniruddha-Gade <gadeaniruddha2@gmail.com>
2025-03-16 23:50:02 +0530

    feat: add console for app listen

--- server/server.ts
+++ server/server.ts
@@ -8,4 +8,5 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log("=================================")
     console.log(`Server started on PORT ${PORT}`)
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 })
